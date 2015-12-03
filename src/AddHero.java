a4e9871a41a2a3b43596fc7401f693d57ba43dd8
Callum Robertson <callum.robertson@atos.net>
2015-12-03 10:59:59 +0000

    More Refactor

--- src/AddHero.java
+++ src/AddHero.java
@@ -6,23 +6,30 @@ import net.atos.entity.SuperHero;
 
 public class AddHero {
 	
-	public String addHero(EntityManager entityManager, Scanner sc) {
-		
+	public String addHero(EntityManager entityManager, Scanner sc){
+		return add(entityManager, sc);
+	}
+	
+	public String add(EntityManager entityManager, Scanner sc) {
 		// This will be replaced by method that will interact with the User's input on website.
+		//Creates an object of my Entity class SuperHero
 		SuperHero hero = new SuperHero();
+		// Sets the Uid in the hero class.
 		hero.setUid(1);
 		System.out.println("Can you please enter the name of the Hero you would like to add: ");
 		String heroName = sc.next();
+		// If the user inputs a nothing this will be printed.
 		if(hero.equals(null) || hero.equals("")){
-			System.err.println(error(entityManager));	
+			String er = error(entityManager);
+			System.out.println(er);	
 		}
 		System.out.println("Hero name: " + heroName);
 		System.out.println("Is your hero part of: ");
 		System.out.println("1: Xmen");
 		System.out.println("2: Avengers");
-			
+		// This takes the input from User and then inputs into DB.	
 		hero.setName(heroName);
-			
+		// Sets the league_id based on user input.
 		int leagueOpt = sc.nextInt();
 		if(leagueOpt == 1){
 			hero.setLeague_id(1);
@@ -30,10 +37,12 @@ public class AddHero {
 		else if(leagueOpt == 2) {
 			hero.setLeague_id(2);
 		}
+		// If the user inputs a nothing this will be printed.
 		else {
-			System.err.println(error(entityManager));	
+			String er = error(entityManager);
+			System.out.println(er);	
 		}
-			
+		// Places the object hero into the DB.	
 		entityManager.persist(hero);
 		entityManager.getTransaction().commit();
 		entityManager.close();
@@ -42,7 +51,6 @@ public class AddHero {
 	}
 	
 	private String error(EntityManager entityManager){
-		
 		entityManager.clear();
 		entityManager.close();
 		System.exit(0);
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 import javax.persistence.*;
-import net.atos.entity.SuperHero;
 
 public class Main {
 	
 	public static void main(String[] args) 
-	{
+	{	// Initialises EntityManagerFactory, Scanner and EntityManager to begin my connection.
 		Scanner sc = new Scanner(System.in);
 		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
 		EntityManager entityManager = emfactory.createEntityManager();
 		entityManager.getTransaction().begin();
+		// Creates an object of Main class so I can call non-static methods.
 		Main m = new Main();
+		// Creates new object of function classes.
 		AddHero add = new AddHero();
 		UpdateHero up = new UpdateHero();
 		DeleteHero del = new DeleteHero();
+		// Calls non-static method to the Main to get initial options from user.
 		m.options(sc, entityManager, add, up, del);
 	}
 
@@ -27,14 +29,17 @@ public class Main {
 		System.out.println("3. Delete a Hero!");
 		int opt = sc.nextInt();
 		if(opt == 1){
+			// Creates a String and puts the returned value from the method into rs.
 			String rs = add.addHero(entityManager, sc);
 			System.out.println(rs);
 		}
 		if(opt == 2){
+			// Creates a String and puts the returned value from the method into rs.
 			String rs = up.updateHero(entityManager, sc);
 			System.out.println(rs);
 		}
 		if(opt == 3){
+			// Creates a String and puts the returned value from the method into rs.
 			String rs = del.deleteHero(entityManager, sc);
 			System.out.println(rs);
 		}
